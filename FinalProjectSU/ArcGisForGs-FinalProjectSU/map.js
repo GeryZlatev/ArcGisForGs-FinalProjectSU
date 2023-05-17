88865dd961de045ab69b4649c32d007d22311845
LeniPetrova99 <133800905+LeniPetrova99@users.noreply.github.com>
2023-05-17 19:01:55 +0300

    commit

---
+++ FinalProjectSU/ArcGisForGs-FinalProjectSU/map.js
@@ -0,0 +1,24 @@
+require([
+    "esri/config",
+    "esri/WebMap",
+    "esri/views/MapView",
+    "esri/widgets/ScaleBar",
+    "esri/widgets/Legend"
+  ], function(esriConfig, WebMap, MapView, ScaleBar, Legend) {
+    esriConfig.apiKey = "AAPK46015e5fe77240af90fba51711bd42bayB9qSDOvnDltAtyyj_zIZdLn6JuDrY7It6ZJnyWx2n8_8COtaFanTJU1FHrvQ-oT";
+    
+    const WebMap = new WebMap ({
+        portalItem: {
+        id: "3ee5ad88b2624b218a71e8f39dd77f92"
+        }
+    
+    })
+
+    const view = new MapView ({
+        container: "viewDiv",
+        map: WebMap 
+        
+    
+    });
+
+})
